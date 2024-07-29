675403bc6ec73cc054d5289924a660b215703db2
egg <happy41072@gmail.com>
2024-07-29 17:57:52 +0800

    Finishing adding

---
+++ LAB_01 - LED Control/main.c
@@ -0,0 +1,134 @@
+#include <nds32_intrinsic.h>
+#include "WT58F2C9.h"
+#include "gpio.h"
+
+
+void delay1(unsigned int nCount)
+{
+   volatile unsigned int i;
+   for (i=0;i<nCount;i++);
+}
+
+
+extern void DRV_Printf(char *pFmt, U16 u16Val);
+
+
+inline void GIE_ENABLE();
+
+void DRV_EnableHWInt (void)
+{
+	/* enable SW0, HW0 and HW1 */
+	__nds32__mtsr(0x10003, NDS32_SR_INT_MASK);
+	/* Enable SW0 */
+	//__nds32__mtsr(0x10000, NDS32_SR_INT_MASK);
+	/* Enable global interrupt */
+	GIE_ENABLE();
+}
+
+void DRV_BlockIntDisable(void)
+{
+	// Disable all interrupt
+	OUTW(rINT0_IE0_ENABLE, 0x0000);
+	OUTW(rINT0_IE1_ENABLE, 0x0000);
+	OUTW(rINT0_IE2_ENABLE, 0x0000);
+	OUTW(rINT1_IE0_ENABLE, 0x0000);
+	OUTW(rINT1_IE1_ENABLE, 0x0000);
+	OUTW(rINT1_IE2_ENABLE, 0x0000);
+}
+
+void DRV_IntInitial(void)
+{
+	// Disable all interrupt
+	DRV_BlockIntDisable();
+
+	// Enable all HW interrupt
+	DRV_EnableHWInt();				//Enable global Hardware interrupt, Assembly command
+
+	// Enable default Block interrupt
+	//DRV_BlockIntEnable();			//Enable each block device interrupt
+
+}
+
+#define rSYS_OPTION1	(0x00200004)
+
+void DRV_SysXtal(U8 u8XtalMode)
+{
+
+	#if(EXTERNAL_XTAL == XTAL_MODE)
+		//-----External Crystal
+		//-----24MHz
+		OUTW(rSYS_OPTION1,((INW(rSYS_OPTION1)&0xFFFFFF00) | 0x0012)); //Use HXTAL and divide 2
+		//-----Crystal 12MHz
+		//OUTW(rSYS_OPTION1, (INW(rSYS_OPTION1) | 0x000A));
+
+		//OUTW(rSYS_OPTION1,(INW(rSYS_OPTION1) | (XTAL<<2) | (HSE_OSC_ON <<1)));
+		//OUTW(rSYS_CLOCK_SELECT,(INW(rSYS_CLOCK_SELECT) | 0x0001));
+	#else
+		//-----Internal RC
+		//-----24MHz
+		//OUTW(rSYS_OPTION1,(INW(rSYS_OPTION1) | 0x0012));
+		//-----Crystal 12MHz
+		//OUTW(rSYS_OPTION1, (INW(rSYS_OPTION1) | 0x000A));
+
+		//OUTW(rSYS_OPTION1,(INW(rSYS_OPTION1) | (XTAL<<2)));
+		//OUTW(rSYS_CLOCK_SELECT,(INW(rSYS_CLOCK_SELECT));
+	#endif
+	//-----MCU Clock Output Test
+	//OUTW(rSYS_OPTION3,0x0090);
+}
+
+void OS_PowerOnDriverInitial(void)
+{
+	//========================= << Typedef Initial  >>
+	//SYS_TypeDefInitial();
+	//========================= << System Clock Initial >>
+	//-----External Crystal
+	DRV_SysXtal(EXTERNAL_XTAL);
+	//========================= << Interrupt Initial >>
+	DRV_IntInitial();
+	//========================= << GPIO Initial >>
+	//DRV_GpioInitial();
+	//========================= << UART Initial >>
+	//DRV_UartInitial();
+	//========================= << Timer Initial >>
+	//DRV_TimerInitial(TIMER_0, SIMPLE_TIMER);
+	//DRV_TimerInitial(TIMER_1, SIMPLE_TIMER);
+	//========================= << PWM Initial >>
+	//DRV_PwmInitial();
+	//========================= << SPI Initial >>
+	//DRV_SpiInitial(SPI_CH2);
+	//========================= << Watchdog Initial >>
+}
+
+
+
+int main()
+{
+	OS_PowerOnDriverInitial();
+
+	GPIO_PTB_DIR = 0x0000;
+	GPIO_PTB_CFG = 0xFFFF;
+	GPIO_PTB_PADIN = 0x00;
+
+	int temp = 0x0001;
+	GPIO_PTB_GPIO = temp;
+	GPIO_PTB_GPIO = GPIO_PTB_GPIO ^ 0xFFFF;
+	delay1(400000);
+	
+	while(1){
+		// GPIO_PTB_GPIO = 0111 1111 1111 1111b ~ 1111 1111 1111 1110b
+		temp = temp << 1;
+		GPIO_PTB_GPIO = temp;
+		GPIO_PTB_GPIO = GPIO_PTB_GPIO ^ 0xFFFF;
+		delay1(400000);
+
+		if(temp == 0x8000){
+			temp = 0x0001;
+			GPIO_PTB_GPIO = temp;
+			GPIO_PTB_GPIO = GPIO_PTB_GPIO ^ 0xFFFF;
+			delay1(400000);
+		}
+	}
+
+	return 0;
+}
